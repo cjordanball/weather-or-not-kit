d80a98bf4139a69784ae5d5ce69249665c403a89
Jordan Ball <cjordanball@ballwebdev.com>
2021-07-18 13:22:16 -0400

    Update Constants.swift

--- weather app/Model/Constants.swift
+++ weather app/Model/Constants.swift
@@ -9,11 +9,6 @@ import Foundation
 
 struct Constants {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather";
-    let appid = "88eb2a1acefe1cd19130e08ce26c1a48";
+    let appid = ""
     let units = "imperial";
 }
-
-
-
-
-//"https://api.openweathermap.org/data/2.5/weather?appid=88eb2a1acefe1cd19130e08ce26c1a48&units=imperial";
