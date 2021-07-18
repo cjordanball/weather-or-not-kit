ed220ecc9d35a2d14015e886f289e55864a88043
Jordan Ball <cjordanball@ballwebdev.com>
2021-07-18 13:43:10 -0400

    remove token

--- weather app/Model/Constants.swift
+++ weather app/Model/Constants.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct Constants {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather";
-    let appid = "84d987ecdb8011c495b7907f99839546";
+    let appid = "";
     let units = "imperial";
 }
